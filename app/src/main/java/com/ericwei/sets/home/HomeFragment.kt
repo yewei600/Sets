892fbd6a60eeae1575923e92d6b4c3bd71e27de9
Eric Wei <ericwei94@gmail.com>
2019-12-18 23:24:08 -0500

    initial rulesfragment

--- app/src/main/java/com/ericwei/sets/home/HomeFragment.kt
+++ app/src/main/java/com/ericwei/sets/home/HomeFragment.kt
@@ -2,13 +2,12 @@ package com.ericwei.sets.home
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
-
 import com.ericwei.sets.R
 import com.ericwei.sets.databinding.FragmentHomeBinding
 
@@ -28,6 +27,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
 
         binding.playBtn.setOnClickListener(this)
         binding.customBtn.setOnClickListener(this)
+        binding.rulesBtn.setOnClickListener(this)
 
         return binding.root
     }
@@ -39,9 +39,8 @@ class HomeFragment : Fragment(), View.OnClickListener {
                 navController.navigate(R.id.action_homeFragment_to_gameFragment)
             R.id.customBtn ->
                 navController.navigate(R.id.action_homeFragment_to_customizeFragment)
-
+            R.id.rulesBtn ->
+                navController.navigate(R.id.action_homeFragment_to_rulesFragment)
         }
-
     }
-
 }
---
+++ app/src/main/java/com/ericwei/sets/rules/RulesFragment.kt
@@ -0,0 +1,25 @@
+package com.ericwei.sets.rules
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ericwei.sets.R
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class RulesFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_rules, container, false)
+    }
+
+
+}
